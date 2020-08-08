9ffd2916ef85ce7219b518c25f0c33ee94e73a9a
ryuunosuke.ishikawa <ryuunosuke113666@yahoo.co.jp>
2020-08-08 15:10:52 +0900

    Add createServlet

--- src/models/validators/UserValidator.java
+++ src/models/validators/UserValidator.java
@@ -1,11 +1,37 @@
 package models.validators;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import models.User;
 import utils.DBUtil;
 
 public class UserValidator {
-        public static String validate(String login_id, Boolean login_id_check_flag) {
+    public static List<String> validate(User u, Boolean login_id_duplicate_check_flag, Boolean password_check_flag) {
+        List<String> errors = new ArrayList<String>();
+
+        String login_id_error = _validateLogin_Id(u.getLogin_Id(), login_id_duplicate_check_flag);
+        if(!login_id_error.equals("")) {
+            errors.add(login_id_error);
+        }
+
+        String name_error = _validateName(u.getName());
+        if(!name_error.equals("")) {
+            errors.add(name_error);
+        }
+
+        String password_error = _validatePassword(u.getPassword(), password_check_flag);
+        if(!password_error.equals("")) {
+            errors.add(password_error);
+        }
+
+        return errors;
+    }
+
+
+        public static String _validateLogin_Id(String login_id, Boolean login_id_check_flag) {
             //必須入力のチェック
             if(login_id == null || login_id.equals("")) {
                 return "ログインIDを入力してください";
