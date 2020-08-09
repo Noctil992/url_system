25fe60cccaface3028b049e3bca90ade93e978a2
ryuunosuke.ishikawa <ryuunosuke113666@yahoo.co.jp>
2020-08-09 15:02:50 +0900

    Add model.url

--- WebContent/WEB-INF/views/users/mypage.jsp
+++ WebContent/WEB-INF/views/users/mypage.jsp
@@ -5,31 +5,16 @@
     <c:param name="content">
      <c:choose>
             <c:when test="${user != null}">
-                <h2>${user.name} さんのマイページ</h2>
+                <h2 class="text-center">${user.name} さんのマイページ</h2><br>
+                <a type="button" class="btn btn-lg btn-primary col-sm-4 col-sm-offset-2" href="#"><span class="badge">New</span>ＵＲＬ新規登録へ</a>
+                <a type="button" class="btn btn-lg btn-danger col-sm-4" href="#"><span class="badge">Delete</span>ＵＲＬ削除へ</a><br>
 
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>氏名</th>
-                            <td><c:out value="${user.name}" /></td>
-                        </tr>
-                        <tr>
-                            <th>権限</th>
-                            <td>
-                                <c:choose>
-                                    <c:when test="${user.admin_flag == 1}">管理者</c:when>
-                                    <c:otherwise>一般</c:otherwise>
-                                </c:choose>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>登録日時</th>
-                            <td>
-                                <fmt:formatDate value="${user.created_at}" pattern="yyyy-MM-dd HH:mm:ss" />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+
+
+
+
+
+
             </c:when>
             <c:otherwise>
                 <h2>お探しのデータは見つかりませんでした。</h2>
---
+++ src/models/Url.java
@@ -0,0 +1,89 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "urls")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllUrls",
+            query = "SELECT u FROM Url AS u ORDER BY u.id DESC"
+            ),
+    @NamedQuery(
+            name = "getUrlsCount",
+            query = "SELECT COUNT(u) FROM Url AS u"
+            ),
+})
+@Entity
+public class Url {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Lob
+    @Column(name = "content", nullable = false)         //コメントのこと
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
